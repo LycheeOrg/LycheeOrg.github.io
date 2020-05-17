7ff925e593d0bf1b6df3c545a22b74bc2b14ab1e
ildyria <beviguier@gmail.com>
2020-05-17 16:43:31 +0200

    settings

--- docs/build.py
+++ docs/build.py
@@ -7,6 +7,7 @@ from markdown.extensions.toc import TocExtension
 import shutil
 
 md = markdown.Markdown(extensions=['extra', TocExtension(baselevel=1, toc_depth=3, anchorlink=True), 'md_in_html'])
+md2 = markdown.Markdown(extensions=['extra', TocExtension(baselevel=1, toc_depth=4, anchorlink=True), 'md_in_html'])
 
 frame = read('template/frame.html')
 
@@ -18,13 +19,18 @@ pages['Directory Structure'] = 'structure'
 pages['Contribution Guide'] = 'contributions'
 pages['Release Notes'] = 'releases'
 pages['API Documentation'] = 'api'
+pages['Settings'] = 'settings'
 
 
 for page_title in pages:
     page_name = pages[page_title]
     text = read('md/' + page_name + '.md')
-    html = md.convert(text)
-    toc = md.toc[17:-7]
+    if page_name == 'settings':
+        html = md2.convert(text)
+        toc = md2.toc[17:-7]
+    else:
+        html = md.convert(text)
+        toc = md.toc[17:-7]
     html = frame.format(content=html, title=page_title, toc=toc)
     save(page_name + '.html', html)
     print(bcolors.GREEN + page_name + bcolors.NORMAL + " generated.")
