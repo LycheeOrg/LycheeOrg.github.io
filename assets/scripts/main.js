9d84670875600405e625ddea42d1970bea394d4b
qwerty287 <ndev@web.de>
2022-11-06 19:41:20 +0100

    Fix feature selection

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,46 @@
-const s = skrollr.init({
+let currentFeatureId = 0;
+function nextFeatureId() {
+	if (currentFeatureId >= 3) {
+		currentFeatureId = 0;
+	} else {
+		currentFeatureId += 1;
+	}
+	return currentFeatureId;
+};
+function selectFeature(t) {
+	const windowContent = document.querySelector(".showcase .window__content");
+	const r = windowContent.src;
+	const o = "" + r.substr(0, r.lastIndexOf("/") + 1) + t + ".jpg";
+	const c = document.querySelector(".showcase__feature[data-id='" + t + "']");
+	windowContent.src = o;
+	document.querySelectorAll(".showcase__feature").forEach(
+		function(t) {
+			t.classList.remove("showcase__feature--active")
+		}
+	);
+	
+	c.classList.add("showcase__feature--active");
+};
+i = setInterval(function() {
+	selectFeature(nextFeatureId())
+},
+4e3);
+document.querySelectorAll(".showcase__feature").forEach(
+	function(feature) {
+		feature.onclick = function(e) {
+			e.preventDefault();
+			clearInterval(i);
+			selectFeature(this.getAttribute("data-id"));
+			return false;
+		}
+	}
+);
+
+
+var n = skrollr.init({
 	forceHeight: false
 });
-if (s.isMobile() || document.body.offsetWidth <= 960) {
-	s.destroy();
+if (n.isMobile() || document.body.offsetWidth <= 960) {
+	n.destroy()
 }
+
