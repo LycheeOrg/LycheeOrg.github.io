d2f55f12a71665a332c06b257e0882554e3748cc
Timo Reymann <Timo.Reymann@trustedshops.de>
2023-06-07 15:57:45 +0200

    style: Reformat code
    - Use format strings for inline usage
    - Add new lines according to python style guide

--- gen.py
+++ gen.py
@@ -12,18 +12,18 @@ from utils.tools import read, save, bcolors
 def numberify_version(v):
     v = v.split('.')
     if len(v[1]) < 2:
-        v[1] = '0'+v[1]
+        v[1] = '0' + v[1]
     if len(v[2]) < 2:
-        v[2] = '0'+v[2]
+        v[2] = '0' + v[2]
     return "".join(v)
 
 
 def generate_base():
-    version = urllib.request.urlopen(
-        "https://raw.githubusercontent.com/LycheeOrg/Lychee/master/version.md").read().decode("utf-8")
+    version_file_url = "https://raw.githubusercontent.com/LycheeOrg/Lychee/master/version.md"
+    version = urllib.request.urlopen(version_file_url).read().decode("utf-8")
     version = version.strip()
 
-    print(bcolors.YELLOW + 'version number: '  + bcolors.NORMAL + version+'\n')
+    print(f'{bcolors.YELLOW}version number: {bcolors.NORMAL} {version}\n')
 
     head = read('template/head.tpl')
     index = read('template/index.tpl')
@@ -42,11 +42,14 @@ def generate_base():
     update_full = update % numberify_version(version)
 
     save("build/index.html", index_full)
-    print(bcolors.GREEN + 'index' + bcolors.NORMAL + " generated.")
+    print(f'{bcolors.GREEN}index{bcolors.NORMAL} generated.')
+
     save("build/support.html", support_full)
-    print(bcolors.GREEN + 'support' + bcolors.NORMAL + " generated.")
+    print(f'{bcolors.GREEN}support{bcolors.NORMAL} generated.')
+
     save("build/update.json", update_full)
-    print(bcolors.GREEN + 'update' + bcolors.NORMAL + " generated.")
+    print(f'{bcolors.GREEN}update{bcolors.NORMAL} generated.')
+
     print("")
 
 
@@ -94,32 +97,33 @@ structure += [['Frequently Asked Question',
 structure += [['Contributing',
                ['contributions', 'api', 'architecture', 'structure', 'frontend', 'livewire']]]
 
+
 def gen_github_link(page):
     html = '<blockquote><p>{tip} Caught a mistake or want to contribute to the documentation?&nbsp;'
-    html += '<a href="https://github.com/LycheeOrg/LycheeOrg.github.io/tree/master/docs/' + page + '.md">Edit this page on Github!</a></p></blockquote>'
+    html += f'<a href="https://github.com/LycheeOrg/LycheeOrg.github.io/tree/master/docs/{page}.md">Edit this page on Github!</a></p></blockquote>'
     return html
 
-def gen_sidebar(page):
 
+def gen_sidebar(page):
     html = ''
     for section in structure:
         html += "<li class='sub--on'>\n"
-        html += "<h2>{}</h2>\n".format(section[0])
+        html += f"<h2>{section[0]}</h2>\n"
         html += "<ul>\n"
         for pages_name in section[1]:
             active = ''
             if pages_name == page:
                 active = " class='active'"
-            html += "<li{active}><a href='{page}.html'>{title}</a></li>\n".format(
-                active=active, page=pages_name, title=pages_title[pages_name])
+            html += f"<li{active}><a href='{page}.html'>{pages_title[pages_name]}</a></li>\n"
         html += "</ul>\n"
 
     return html
 
+
 def generate_doc():
     for page_name in pages_title:
         page_title = pages_title[page_name]
-        text = read('docs/' + page_name + '.md')
+        text = read(f'docs/{page_name}.md')
         if page_name == 'settings':
             html = md2.convert(text)
             toc = md2.toc[17:-7]
@@ -131,37 +135,36 @@ def generate_doc():
         html += gen_github_link(page_name)
 
         html = frame.format(content=html, title=page_title, toc=toc, aside=aside)
-        save('build/docs/' +  page_name + '.html', html)
-        print(bcolors.GREEN + page_name + bcolors.NORMAL + " generated.")
+        save(f'build/docs/{page_name}.html', html)
+        print(f'{bcolors.GREEN} {page_name}{bcolors.NORMAL} generated.')
 
     shutil.copy('build/docs/installation.html', 'build/docs/index.html')
-
 
 
 def check():
     repo = Repo.init('.')
     if repo.is_dirty():  # check the dirty state
         for item in repo.index.diff(None):
-            print(bcolors.RED + item.a_path + " changed." + bcolors.NORMAL)
+            print(f'{bcolors.RED} {item.a_path}  changed.{bcolors.NORMAL}')
         print("")
-        print(bcolors.RED + "Please commit them." + bcolors.NORMAL)
+        print(f'{bcolors.RED}Please commit them. {bcolors.NORMAL}')
         return True
     else:
-        print(bcolors.GREEN + "No changes detected." + bcolors.NORMAL)
+        print(f'{bcolors.GREEN}No changes detected.{bcolors.NORMAL}')
         return False
-
 
 
 def test_main():
     generate_base()
     generate_doc()
-    assert(not check())
+    assert (not check())
 
 
 def main():
     generate_base()
     generate_doc()
     check()
+
 
 if __name__ == '__main__':
     main()
